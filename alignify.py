cc6ca6712503709461b91daef5117bd796064cf7
Emil Ernerfeldt <emilernerfeldt@gmail.com>
2013-01-16 13:01:16 +0100

    Commented code

--- alignify.py
+++ alignify.py
@@ -1,25 +1,33 @@
 #!/usr/bin/python -O
 
+# Made by Emil Ernerfeldt (emil ernerfeldt at gmail . com)
+# https://github.com/emilk/alignify
+# Feel free to use, abuse, modify, redistribute.
+# If you like Alignify, send me a mail and tell me!
+# Oh, and this is my first python script, so beware of ugly code =)
+
 # Usage:  cat test.txt | python alignify.py
 # Or:     python alignify.py test.txt
 
-import fileinput
-import re
 
+import fileinput  # reads from stdin or from file given as argument
+import re         # reg-ex
+
+# For debugging
 def spam(*stuff):
 	#print 'SPAM: ', ''.join(map(str,stuff))
 	pass
 	
+	
 def output(*stuff):
-	#print 'OUTPUT:                                             |', ''.join(map(str,stuff))
 	print ''.join(map(str,stuff))
-	pass
 
 
 def main():
-	indents = []
-	lines = []
-	indent = ""
+	indents  = []
+	lines    = []
+	indent   = ""
+	
 	for line in fileinput.input():
 		spam("Read line: ", line)
 		m = re.match(r"(\t*)(.*)", line)
@@ -34,7 +42,7 @@ def main():
 			spam("new block (old tabs: ", len(indent), ", now: ", len(tabs), ")")
 			parse_block(indents, lines)
 			indents  = []
-			lines  = []
+			lines    = []
 	
 		spam("APPENDING")
 		indents.append(tabs)
@@ -45,14 +53,15 @@ def main():
 	return
 
 
+# Takes a number of lines all with the same indentation
 def parse_block(indents, lines):
 	if len(lines)==0:
 		return
 	spam("parse_block: ", len(lines))
 	# Break up each line into beginning, spacing, and end.
-	begs = []
-	spaces = []
-	ends = []
+	begs    = []
+	spaces  = []
+	ends    = []
 	for ix, line in enumerate(lines):
 		m = re.match(r"(.*?)(\s\s+)(.*)", line)
 		if m!=None:
@@ -64,15 +73,16 @@ def parse_block(indents, lines):
 			# No spacing in this line - handle preceding lines and continue:
 			spam("NO MATCH")
 			handle_block(begs, spaces, ends)
-			begs = []
-			spaces = []
-			ends = []
+			begs    = []
+			spaces  = []
+			ends    = []
 			output(indents[ix], line)
 	spam("LOOP END")
 	handle_block(begs, spaces, ends)
 	return
 
 
+# Takes a number of lines split by the first spacing (two or more tabs)
 def handle_block(begs, spaces, ends):
 	if len(begs)==0:
 		return
@@ -87,12 +97,12 @@ def handle_block(begs, spaces, ends):
 	align = len(longest_beg) + len(longest_spaces)
 	spam("align: ", align)
 	
-	new_begs = []
-	new_lines = []
+	new_begs   = []
+	new_lines  = []
 	
 	for ix, beg in enumerate(begs):
 		pad_len = (align - len(beg))
-		pad = " " * (align - len(beg))
+		pad = " " * pad_len
 		spam("pad_len: ", pad_len)
 		#output(begs[ix], pad, ends[ix])
 		new_begs.append(begs[ix] + pad)
